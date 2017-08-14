40f84e82750c334c57fcb6e9a009342679a17536
saralein <sara.m.trujillo@gmail.com>
2017-08-14 11:53:14 -0500

    Move second display_board test into correct block (was outside of the block.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,7 +32,7 @@ describe Board do
   let(:size_three_board) { Board.new(3)}
   let(:size_four_board) { Board.new(4) }
 
-  describe 'createNewBoard' do
+  describe 'create_new_board' do
     it 'creates a nested array based on board size' do
       size_three_board.create_new_board
       expect(size_three_board.moves).to eql(size_three_moves)
@@ -41,17 +41,18 @@ describe Board do
     end
   end
 
-  describe 'displayBoard' do
+  describe 'display_board' do
     it 'displays the current sized board in the terminal' do
       size_three_board.moves = size_three_moves
       expect(STDOUT).to receive(:puts).with(display_output)
       size_three_board.display_board
     end
+
+    it 'displays the current size board in the terminal with moves' do
+      size_three_board.moves = size_three_moves_taken
+      expect(STDOUT).to receive(:puts).with(display_output_with_moves)
+      size_three_board.display_board
+    end
   end
 
-  it 'displays the current size board in the terminal with moves' do
-    size_three_board.moves = size_three_moves_taken
-    expect(STDOUT).to receive(:puts).with(display_output_with_moves)
-    size_three_board.display_board
-  end
 end
