a06e0a87c8f389c64a30c9136ef12c57da70ebb8
saralein <sara.m.trujillo@gmail.com>
2017-08-14 10:37:36 -0500

    Add tests and code for displayBoard method on Board class.

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,28 @@
 class Board
-  attr_accessor :board
+  attr_accessor :moves
 
-  def initialize
-    @board
+  def initialize(size)
+    @moves
+    @size = size
   end
-  def createNewBoard(size)
-    @board = Array.new(size){ Array.new(size){'-'} }
+
+  def createNewBoard
+    @moves = Array.new(@size){ Array.new(@size){'-'} }
+  end
+
+  def displayBoard
+    output = ''
+    @moves.each_with_index do |row, row_index|
+      row.each_with_index do |spot, spot_index|
+        output += spot == '-' ? ' ' : spot
+        if (spot_index != @size - 1)
+          output += '|'
+        end
+      end
+      if (row_index != @size - 1)
+        output += "\n - + - + - \n"
+      end
+    end
+    puts output
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,25 +1,57 @@
 require_relative '../lib/board.rb'
 
 describe Board do
-  let(:size_three_board) { [
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-']
-  ] }
-  let(:size_four_board) { [
-    ['-', '-', '-', '-'],
-    ['-', '-', '-', '-'],
-    ['-', '-', '-', '-'],
-    ['-', '-', '-', '-']
-  ] }
-  let(:board) { Board.new }
+  let(:size_three_moves) {
+    [
+      ['-', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-']
+    ]
+  }
+  let(:size_four_moves) {
+    [
+      ['-', '-', '-', '-'],
+      ['-', '-', '-', '-'],
+      ['-', '-', '-', '-'],
+      ['-', '-', '-', '-']
+    ]
+  }
+  let(:size_three_moves_taken) {
+    [
+      ['O', 'X', '-'],
+      ['-', 'O', '-'],
+      ['-', '-', 'X']
+    ]
+  }
+  let(:display_output) {
+    " | | \n - + - + - \n | | \n - + - + - \n | | "
+  }
+  let(:display_output_with_moves) {
+    "O|X| \n - + - + - \n |O| \n - + - + - \n | |X"
+  }
+  let(:size_three_board) { Board.new(3)}
+  let(:size_four_board) { Board.new(4) }
 
   describe 'createNewBoard' do
     it 'creates a nested array based on board size' do
-      board.createNewBoard(3)
-      expect(board.board).to eql(size_three_board)
-      board.createNewBoard(4)
-      expect(board.board).to eql(size_four_board)
+      size_three_board.createNewBoard
+      expect(size_three_board.moves).to eql(size_three_moves)
+      size_four_board.createNewBoard
+      expect(size_four_board.moves).to eql(size_four_moves)
     end
+  end
+
+  describe 'displayBoard' do
+    it 'displays the current sized board in the terminal' do
+      size_three_board.moves = size_three_moves
+      expect(STDOUT).to receive(:puts).with(display_output)
+      size_three_board.displayBoard
+    end
+  end
+
+  it 'displays the current size board in the terminal with moves' do
+    size_three_board.moves = size_three_moves_taken
+    expect(STDOUT).to receive(:puts).with(display_output_with_moves)
+    size_three_board.displayBoard
   end
 end
