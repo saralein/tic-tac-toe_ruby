158606b28300ab3c1cc431f9cc506627ae84e88d
saralein <sara.m.trujillo@gmail.com>
2017-08-20 11:43:10 -0500

    Refactors board and board checker classes.

--- lib/board/board_checker.rb
+++ lib/board/board_checker.rb
@@ -1,76 +1,49 @@
 class BoardChecker
-  def is_game_over
-    return there_is_full_diagonal ||
-           there_is_full_row ||
-           there_is_full_column ||
-           is_draw
+  attr_reader :empty_char, :winner
+  def initialize(size, empty_char)
+    @size = size
+    @empty_char = empty_char
+    @winner = ''
   end
 
-  def is_draw
-    no_spots_left = @moves.select{ |spot| spot === @empty_char }.length == 0
-
-    @winner = no_spots_left ? '-' : nil
-
-    return no_spots_left
+  def game_over?(grid, turns_remaining)
+    return winner?(grid) || draw?(turns_remaining)
   end
 
-  def there_is_full_row
-    is_won = false
+  def winner?(grid)
+    rows = get_rows(grid)
+    patterns = rows.push(*get_columns(rows), *get_diagonals(rows))
 
-    for i in 0...@size
-      row_start = @size * i
-      row_end = @size*(i+1)
-      row = @moves[row_start...row_end]
-      is_won = is_winner(row)
+    winner = get_winner(patterns)
+    set_winner(winner)
 
-      if (is_won)
-        return is_won
-      end
-    end
-
-    return is_won
+    return winner != ''
   end
 
-  def there_is_full_column
-    is_won = false
-
-    for i in 0...@size
-      column = []
-      for j in 0...@size
-        column.push(@moves[i + @size*j])
-      end
-      is_won = is_winner(column)
-      if(is_won)
-        return is_won
-      end
-    end
-
-    return is_won
+  def draw?(turns_remaining)
+    turns_remaining == 0
   end
 
-  def there_is_full_diagonal
-    left_diagonal = []
-    right_diagonal = []
-
-    for i in 0...@size
-      left_diagonal.push(@moves[(@size+1) * i])
-      right_diagonal.push(@moves[(@size-1) * (1 + i)])
-    end
-
-    is_left_won = is_winner(left_diagonal)
-    is_right_won = is_winner(right_diagonal)
-
-    return is_left_won || is_right_won
+  def get_rows(grid)
+    grid.each_slice(@size).to_a
   end
 
-  def is_winner(row_array)
-    token = row_array[0]
-    is_winner = row_array.uniq.length == 1 && token != @empty_char
+  def get_columns(rows)
+    rows.transpose
+  end
 
-    if (is_winner)
-      @winner = token
-    end
+  def get_diagonals(rows)
+    [] << rows.each_with_index.map { |row, i| row[i]} <<
+          rows.each_with_index.map { |row, i| row[(@size - 1) - i]}
+  end
 
-    return is_winner
+  def get_winner(patterns)
+    winning_pattern = patterns.each.select { |pattern| pattern.uniq.length == 1 && pattern[0] != @empty_char }.flatten
+
+    return winning_pattern.length == 0 ? '' : winning_pattern[0]
+  end
+
+  def set_winner(winner)
+    @winner = winner
   end
 end
