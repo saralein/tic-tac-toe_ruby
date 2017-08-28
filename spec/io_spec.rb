67b027956ddb2df38f620994b8ad00fc82db5453
saralein <sara.m.trujillo@gmail.com>
2017-08-28 12:31:40 -0500

    Combines tests and updates test decriptions.

--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -8,26 +8,18 @@ describe 'IO' do
   let(:io) { IO.new(input, output) }
 
 
-  describe 'display_user_message' do
-    it 'calls the print method on stdout' do
+  describe 'display_message' do
+    it 'displays a given message in the UI' do
       io.display_message('message')
       expect(output.times_called).to eql(1)
-    end
-
-    it 'receives a messages to display and sends to stdout' do
-      io.display_message('message')
       expect(output.check_message).to eql('message')
     end
   end
 
-  describe 'get_user_input' do
-    it 'calls the gets method on stdin' do
+  describe 'get_input' do
+    it 'receives input from the user' do
       io.get_input
       expect(input.times_called).to eql(1)
-    end
-
-    it 'receives user input from stdin' do
-      io.get_input
       expect(input.gets).to eql('input')
     end
   end
